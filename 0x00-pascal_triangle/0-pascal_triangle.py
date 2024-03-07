11a6d6cc7d6ed0a9a9c34ca690b773695d7a5bca
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-03-07 20:19:04 +0000

    Implemented pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Pascals triangle
+"""
+
+
+def pascal_triangle(n):
+    triangle = []
+
+    if n <= 0:
+        return triangle
+
+    triangle.append([1])
+
+    for i in range(1, n):
+        temp = [1]
+        for j in range(1, i):
+            s = triangle[i-1][j-1]
+            t = triangle[i-1][j]
+            temp.append(s+t)
+        temp.append(1)
+        triangle.append(temp)
+    return triangle
