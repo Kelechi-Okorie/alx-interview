b92c8fb84a6d6c1ce4625684906aa623b4a9a18e
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-03-17 20:51:42 +0000

    Added task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Lockboxes implementation
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened
+
+    parameters:
+    - boxes (list): list of all the boxes
+
+    Returns 
+    - True if all boxes can be opened, False otherwise
+    """
+
+    visited = [False] * len(boxes)
+    visited[0] = True
+    keys = [0]
+
+    while keys:
+        cur_key = keys.pop()
+        cur_box = boxes[cur_key]
+        
+        for key in cur_box:
+            if 0 <= key < len(boxes) and not visited[key]:
+                visited[key] = True
+                keys.append(key)
+    
+    # Check if all boxes have been visited
+    return all(visited)
